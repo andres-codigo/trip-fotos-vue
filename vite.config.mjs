fe0a0eeba5a60f48f9304326acfc2e67f801e0e4
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-02-14 12:30:32 +1100

    [*config.js, *config.mjs] port updates
    - cypress config/vite config: ports updated to not clash with other projects

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,11 +6,11 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
-		port: 3000,
+		port: 4000,
 		open: true,
 	},
 	preview: {
-		port: 3001,
+		port: 4001,
 		open: true,
 	},
 	plugins: [vue(), eslintPlugin()],
